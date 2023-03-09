44a48eeb31abf79d546c14130f9b7d7d41cdc24a
Allan Kiche <tle2819@students.mu.ac.ke>
2023-03-09 15:54:28 +0300

    hero

---
+++ lib/widgets/hero_image.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HeroImage extends StatelessWidget {
+  const HeroImage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Hero(
+      tag: 'agriculture-icon',
+      child: Container(
+        height: 200,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/agriculture-icon.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
