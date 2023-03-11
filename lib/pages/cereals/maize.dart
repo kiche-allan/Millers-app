69cbbfc6bc2a6d5fc51736c8cc2eb909d320f96b
Allan Kiche <tle2819@students.mu.ac.ke>
2023-03-11 21:48:59 +0300

    maize

--- lib/pages/cereals/maize.dart
+++ lib/pages/cereals/maize.dart
@@ -1,4 +1,9 @@
-import 'package:flutter/material.dart';
+
+
+
+ import 'package:flutter/material.dart';
+import 'package:ecommerce/models/doctor_model.dart';
+import 'package:ionicons/ionicons.dart';
 
 class MaizePage extends StatelessWidget {
   const MaizePage({Key? key}) : super(key: key);
@@ -23,156 +28,137 @@ class MaizePage extends StatelessWidget {
             ),
             const SizedBox(height: 20),
             Expanded(
-              child: ListView.builder(
-                itemCount: 4,
-                scrollDirection: Axis.horizontal,
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 20,
+                  crossAxisSpacing: 20,
+                  childAspectRatio: 0.75,
+                ),
+                itemCount: null,
                 itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.only(right: 20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          width: 100,
-                          height: 100,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.grey[300],
-                          ),
-                          child: const Icon(Icons.person),
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 150,
+                        height: 150,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.grey[300],
                         ),
-                        const SizedBox(height: 10),
-                        const Text(
-                          'Farmer Name',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
+                        child: const Icon(Icons.person),
+                      ),
+                      const SizedBox(height: 10),
+                      const Text(
+                        'Farmer Name',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
                         ),
-                        const SizedBox(height: 5),
-                        Row(
-                          children: [
-                            const Icon(
-                              Icons.star,
-                              color: Colors.amber,
-                              size: 16,
-                            ),
-                            const Icon(
-                              Icons.star,
-                              color: Colors.amber,
-                              size: 16,
-                            ),
-                            const Icon(
-                              Icons.star,
-                              color: Colors.amber,
-                              size: 16,
-                            ),
-                            const Icon(
-                              Icons.star_border,
-                              color: Colors.amber,
-                              size: 16,
-                            ),
-                            const Icon(
-                              Icons.star_border,
-                              color: Colors.amber,
-                              size: 16,
-                            ),
-                            const SizedBox(width: 5),
-                            const Text('4.0 (10)')
-                          ],
-                        )
-                      ],
-                    ),
+                      ),
+                      const SizedBox(height: 5),
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.star,
+                            color: Colors.amber,
+                            size: 16,
+                          ),
+                          const Icon(
+                            Icons.star,
+                            color: Colors.amber,
+                            size: 16,
+                          ),
+                          const Icon(
+                            Icons.star,
+                            color: Colors.amber,
+                            size: 16,
+                          ),
+                          const Icon(
+                            Icons.star_border,
+                            color: Colors.amber,
+                            size: 16,
+                          ),
+                          const Icon(
+                            Icons.star_border,
+                            color: Colors.amber,
+                            size: 16,
+                          ),
+                          const SizedBox(width: 5),
+                          const Text('4.0 (10)')
+                        ],
+                      )
+                    ],
                   );
                 },
               ),
             ),
-            const SizedBox(height: 20),
-            const Text(
-              'Maize Products',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
-            ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 30),
             Expanded(
-              child: GridView.count(
-                crossAxisCount: 2,
-                children: List.generate(
-                  4,
-                  (index) {
-                    return Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.grey[300],
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 20,
+                  crossAxisSpacing: 20,
+                  childAspectRatio: 0.75,
+                ),
+                itemCount: null,
+                itemBuilder: (context, index) {
+                  return Row(
+                    children: [
+                      Container(
+                        width: 150,
+                        height: 150,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                            image: AssetImage(nearbyDoctors[index].profile),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       ),
-                      child: Column(
+                      const SizedBox(width: 10),
+                      Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Expanded(
-                            child: Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                image: const DecorationImage(
-                                  image: NetworkImage(
-                                      'https://via.placeholder.com/150'),
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            ),
+                          Text(
+                            "Dr. ${nearbyDoctors[index].name}",
+                            style: const TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
                           ),
-                          const SizedBox(height: 10),
-                          const Text(
-                            'Maize Product Name',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          const SizedBox(height: 5),
+                          const SizedBox(height: 8),
+                          const Text("General Farmers"),
+                          const SizedBox(height: 16),
                           Row(
                             children: [
-                              const Icon(
-                                Icons.star,
-                                color: Colors.amber,
-                                size: 16,
+                              Icon(
+                                Ionicons.star,
+                                color: Colors.yellow[700],
+                                size: 18,
                               ),
-                              const Icon(
-                                Icons.star,
-                                color: Colors.amber,
-                                size: 16,
+                              const Padding(
+                                padding:
+                                    EdgeInsets.only(left: 4, right: 6),
+                                child: Text(
+                                  "4.0",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold),
+                                ),
                               ),
-                              const Icon(
-                                Icons.star,
-                                color: Colors.amber,
-                                size: 16,
-
-                              ),
-                              const Icon(
-                                Icons.star_border,
-                                color: Colors.amber,
-                                size: 16,
-                              ),
-                              const Icon(
-                                Icons.star_border,
-                                color: Colors.amber,
-                                size: 16,
-                              ),
-                              //finish the code
-                              const SizedBox(width: 5),
-                              const Text('4.0 (10)')
+                              const Text("195 Reviews")
                             ],
                           )
                         ],
-                      ),
-                    );
-                  },
-                ),
+                      )
+                    ],
+                  );
+                },
               ),
             ),
           ],
         ),
-
       ),
     );
   }
 }
+                
