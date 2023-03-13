6186d4acc390ddf064e659d42edd804da848b629
Allan Kiche <tle2819@students.mu.ac.ke>
2023-03-13 09:47:37 +0300

    wheat1

---
+++ lib/models/MilletModel.dart
@@ -0,0 +1,69 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class MilletModel {
+final String name;
+final String position;
+final int averageReview;
+final int totalReviews;
+final String profile;
+final String location;
+MilletModel({
+required this.name,
+required this.position,
+required this.averageReview,
+required this.totalReviews,
+required this.profile,
+required this.location,
+});
+}
+
+final List<MilletModel> nearbyFarmers = [
+MilletModel(
+name: "Samuel Ochieng",
+position: "Maize Farmer",
+averageReview: 0,
+totalReviews: 0,
+profile: "assets/doctor_1.jpg",
+location: "Kesses",
+),
+MilletModel(
+name: "Yonah Oketch",
+position: "Maize Farmer",
+averageReview: 0,
+totalReviews: 0,
+profile: "assets/doctor_2.jpg",
+location: "Cattle Dip",
+),
+MilletModel(
+name: "Shadrack Bii",
+position: "Maize Farmer",
+averageReview: 2,
+totalReviews: 0,
+profile: "assets/doctor_3.jpg",
+location: "Moi University",
+),
+MilletModel(
+name: "Danton Onyango",
+position: "Maize Farmer",
+averageReview: 0,
+totalReviews: 0,
+profile: "assets/doctor_1.jpg",
+location: "Kesses",
+),
+
+MilletModel(
+name: "Vivian Koech",
+position: "Maize Farmer",
+averageReview: 2,
+totalReviews: 0,
+profile: "assets/doctor_3.jpg",
+location: "Moi University",
+),
+MilletModel(
+name: "Lilian Grace",
+position: "Maize Farmer",
+averageReview: 0,
+totalReviews: 0,
+profile: "assets/doctor_1.jpg",
+location: "Kesses",
+),
+];
